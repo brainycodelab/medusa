23e37d8c9fe6b420b0e142af6ced722eb3587b21
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-01-09 13:45:40 +0100

    Implement FailOnRevert

--- fuzzing/test_case_assertion_provider.go
+++ fuzzing/test_case_assertion_provider.go
@@ -1,13 +1,15 @@
 package fuzzing
 
 import (
+	"sync"
+
 	"github.com/crytic/medusa/compilation/abiutils"
 	"github.com/crytic/medusa/fuzzing/calls"
 	"github.com/crytic/medusa/fuzzing/config"
 	"github.com/crytic/medusa/fuzzing/contracts"
 	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/core/vm"
 	"golang.org/x/exp/slices"
-	"sync"
 )
 
 // AssertionTestCaseProvider is am AssertionTestCase provider which spawns test cases for every contract method and
@@ -70,6 +72,13 @@ func (t *AssertionTestCaseProvider) checkAssertionFailures(callSequence calls.Ca
 	// want to be backwards compatible with older Solidity which simply hit an invalid opcode and did not actually
 	// have a panic code.
 	lastExecutionResult := lastCall.ChainReference.MessageResults().ExecutionResult
+
+	// Check for revert or require failures if FailOnRevert is set to true
+	if t.fuzzer.config.Fuzzing.Testing.AssertionTesting.AssertionModes.FailOnRevert {
+		if lastExecutionResult.Err == vm.ErrExecutionReverted {
+			return &methodId, true, nil
+		}
+	}
 	panicCode := abiutils.GetSolidityPanicCode(lastExecutionResult.Err, lastExecutionResult.ReturnData, true)
 	failure := false
 	if panicCode != nil {
