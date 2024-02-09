5a1e2e832420e2c8d5e057fce5c5237b38f1f138
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-02-09 09:02:08 +0100

    Add receive function in test contract

--- fuzzing/testdata/contracts/deployments/starting_balance.sol
+++ fuzzing/testdata/contracts/deployments/starting_balance.sol
@@ -1,5 +1,7 @@
 // DeployedContractStartingBalance checks the balance of a contract after deployment to ensure the starting balance was properly set
 contract DeployedContractStartingBalance {
+    receive() external payable {}
+
     function checkBalance() public {
         assert(address(this).balance == 3000);
     }
